46f432b6c4b1dbd7d3278d9f1164f7cbcddf8077
Wendy Verbruggen <wendy.verbruggen@swrve.com>
2013-09-11 14:43:37 +0100

    End of chapter 10

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  has_many :microposts, dependent: :destroy
+
   before_save { email.downcase! }
   before_create :create_remember_token
 
@@ -11,6 +13,10 @@ class User < ActiveRecord::Base
   validates :password, length: { minimum: 6 }
   
   has_secure_password
+
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
 
   def User.new_remember_token
     SecureRandom.urlsafe_base64
